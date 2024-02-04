2566422619b3920b1102690d3d6aea63515c7d4c
Hamachi-Multi <dptclsrn70@gmail.com>
2024-02-04 22:24:36 +0900

    버그 수정 2

--- ARAM_TD.js
+++ ARAM_TD.js
@@ -160,7 +160,7 @@ async function connectAsMain(button) {
             })
         })
     .catch(async () => {  
-        switchButtonText(button);
+        await switchButtonText(button);
     });
     
     connectionLock = false;
